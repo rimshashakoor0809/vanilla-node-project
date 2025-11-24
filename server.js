67a722cea27cb9f80895a7fa20ad9fe6112ab3cb
Rimsha Shakoor <rimsha.shakoor0809@gmail.com>
2025-11-24 20:54:18 +0500

    feat: add test and users endpoints for testing purpose

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 import { createServer } from 'http';
 import path from 'path';
+import { Router } from './core/Router.js';
 
 const PORT = process.env.PORT || 2000;
 const server = createServer();
+const router = new Router();
+
+router.get('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  res.end('Users' + id)
+})
+router.get('/api/:slug/test', (req, res) => res.end('Test Endpoint'))
+
+server.on('request', (req, res, next) => {
+  router.handler(req, res, next);
+
+})
 
 
 server.listen(PORT, () => console.log(`Server is listening at port: ${PORT}`));
